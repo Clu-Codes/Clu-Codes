df732e37ea219b7c6f64bbdc24449499354bb1b0
Clu <retsulcconnor@gmail.com>
2021-02-27 13:57:49 -0500

    update portfolio

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// import Masonry from 'masonry-layout';
+
+// window.onload = () => {
+//     const grid = document.querySelector('.grid');
+
+//     const masonry = new Masonry(grid, {
+//         itemSelector: '.grid-item',
+//         gutter: 2,
+//         originLeft: false,
+//         columnWidth: 200,
+//         percentPosition: true,
+
+//     });
+// }
